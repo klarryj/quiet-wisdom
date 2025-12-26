b66f311c097a9602bc5141e4068bae967ce616de
klarryj <lpcft01@gmail.com>
2025-12-26 20:18:10 +0300

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,20 @@
+import { auth, db } from "./firebase.js";
+import {
+  addDoc, collection
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+submit.onclick = async () => {
+  if (!auth.currentUser) return;
+
+  await addDoc(collection(db, "posts"), {
+    title: title.value,
+    body: body.value,
+    location: location.value,
+    authorName: "Anonymous",
+    createdAt: Date.now(),
+    published: true
+  });
+
+  msg.innerText = "Published successfully.";
+};
 
