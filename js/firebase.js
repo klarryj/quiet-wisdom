3a7292be03d1dfe67175b12d4d2a05aac1f5507f
klarryj <lpcft01@gmail.com>
2025-12-26 20:06:04 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG32pjVQX60RwKvbLZiSK0P9mr3bGa_Jo",
+  authDomain: "quiet-wisdom.firebaseapp.com",
+  projectId: "quiet-wisdom",
+  storageBucket: "quiet-wisdom.firebasestorage.app",
+  messagingSenderId: "793350811214",
+  appId: "1:793350811214:web:40b672ea449beb10fe9d1f"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
